4bcb7d08cc0e88a91ea0ead5b0a3c091e5641a7c
ldaiane <ldaianestefanie@gmail.com>
2026-03-09 23:31:21 -0300

    Add loadFolder function to process media files

--- js/media.js
+++ js/media.js
@@ -1,1 +1,25 @@
+function loadFolder(files){
 
+for(let file of files){
+
+if(file.name.endsWith(".txt")){
+
+file.text().then(parseWhatsApp)
+
+}else{
+
+currentChat.messages.push({
+
+sender:"arquivo",
+date:new Date(),
+media:file
+
+})
+
+}
+
+}
+
+renderChat()
+
+}

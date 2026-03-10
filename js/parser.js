4486f8c9946735b2c922e82cb95cc3573c3ca5ab
ldaiane <ldaianestefanie@gmail.com>
2026-03-09 23:30:35 -0300

    Add WhatsApp message parsing functionality

--- js/parser.js
+++ js/parser.js
@@ -1,1 +1,34 @@
+function parseWhatsApp(text){
 
+const lines = text.split("\n")
+
+for(let line of lines){
+
+const regex=/(\d{2}\/\d{2}\/\d{4}), (\d{2}:\d{2}) - (.*?): (.*)/
+
+const match=line.match(regex)
+
+if(match){
+
+const date=match[1]
+const time=match[2]
+const sender=match[3]
+const message=match[4]
+
+currentChat.messages.push({
+
+sender:sender,
+text:message,
+date:new Date(date+" "+time)
+
+})
+
+}
+
+}
+
+sortMessages()
+
+renderChat()
+
+}
